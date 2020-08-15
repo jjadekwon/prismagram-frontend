fb01f62136c245a4c5bac9b5b1e9435e548e89a4
jjadekwon <jjade.kwon@gmail.com>
2020-08-15 15:37:26 +0900

    #4.3 Apollo Client

---
+++ src/Apollo/LocalState.js
@@ -0,0 +1,22 @@
+export const defaults = {
+    isLoggedIn: localStorage.getItem("token") !== null ? true : false
+}
+
+export const resolvers = {
+    Mutation: {
+        logUserIn: (_, { token }, { cache }) => {
+            localStorage.setItem("token", token);
+            cache.writeData({
+                data: {
+                    isLoggedIn: true
+                }
+            });
+            return null;
+        },
+        logUserOut: (_, __, { cache }) => {
+            localStorage.removeItem("token");
+            window.location.reload(); // 전체 페이지 리로드, 모든 cache를 없앰
+            return null;
+        }
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloProvider } from "react-apollo-hooks";
 import App from './Components/App';
+import Client from './Apollo/Client';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={Client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
