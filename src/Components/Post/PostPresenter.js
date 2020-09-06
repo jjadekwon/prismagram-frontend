85c6478b8d349fedc1977692dabec1a100ccceab
jjadekwon <jjade.kwon@gmail.com>
2020-09-06 23:25:47 +0900

    #9.1 Profile Screen

--- src/Components/Post/PostPresenter.js
+++ src/Components/Post/PostPresenter.js
@@ -4,6 +4,7 @@ import TextareaAutosize from "react-autosize-textarea";
 import FatText from "../FatText";
 import Avatar from "../Avatar";
 import { HeartFull, HeartEmpty, Comment as CommentIcon } from "../Icons";
+import { Link } from "react-router-dom";
 
 const Post = styled.div`
   ${(props) => props.theme.whiteBox}
@@ -11,6 +12,9 @@ const Post = styled.div`
   max-width: 600px;
   margin-bottom: 25px;
   user-select: none;
+  a {
+    color: inherit;
+  }
 `;
 
 const Header = styled.header`
@@ -89,6 +93,10 @@ const Textarea = styled(TextareaAutosize)`
   }
 `;
 
+const Caption = styled.div`
+  margin: 10px 0px;
+`;
+
 const Comments = styled.ul`
   margin-top: 10px;
 `;
@@ -112,13 +120,16 @@ export default ({
   toggleLike,
   onKeyPress,
   comments,
-  selfComments
+  selfComments,
+  caption,
 }) => (
   <Post>
     <Header>
       <Avatar size="sm" url={avatar} />
       <UserColumn>
-        <FatText text={username} />
+        <Link to={`/${username}`}>
+          <FatText text={username} />
+        </Link>
         <Location>{location}</Location>
       </UserColumn>
     </Header>
@@ -138,6 +149,9 @@ export default ({
         </Button>
       </Buttons>
       <FatText text={likeCount === 1 ? "1 like" : `${likeCount} likes`} />
+      <Caption>
+        <FatText text={username} /> {caption}
+      </Caption>
       {comments && (
         <Comments>
           {comments.map((comment) => (
@@ -154,7 +168,7 @@ export default ({
           ))}
         </Comments>
       )}
-      
+
       <Timestamp>{createdAt}</Timestamp>
       <Textarea
         placeholder="Add a comment..."
